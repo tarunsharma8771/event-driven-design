759fb69f5a18ac5bfcec0afc5c54549f6a962ba7
Tarun Sharma <TarunSharma8771@gmail.com>
2026-06-29 10:19:51 +0530

    Initial commit: Event-driven design project

---
+++ inventory-service/src/main/java/org/example/InventoryServiceApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
